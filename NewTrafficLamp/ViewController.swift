7fa05b313061d03e1c439aa293103d2902c69fd6
Andrey Klimenko <45719199+KlimEnN@users.noreply.github.com>
2021-08-20 21:06:09 +0300

    new

--- NewTrafficLamp/ViewController.swift
+++ NewTrafficLamp/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLamp {
+    case red, yellow, green
+}
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var red: UIView!
+    @IBOutlet weak var yellow: UIView!
+    @IBOutlet weak var green: UIView!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    private var currentLamp = CurrentLamp.red
+    private let lampOn: CGFloat = 1
+    private let lampOff: CGFloat = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 10
+        
+        red.alpha = lampOff
+        yellow.alpha = lampOff
+        green.alpha = lampOff
     }
 
+    override func viewDidLayoutSubviews() {
+        red.layer.cornerRadius = red.frame.width / 2
+        yellow.layer.cornerRadius = yellow.frame.width / 2
+        green.layer.cornerRadius = green.frame.width / 2
+    }
+    
+    @IBAction func startButton() {
+        if button.currentTitle == "START" {
+            button.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLamp {
+        case .red:
+            green.alpha = lampOff
+            yellow.alpha = lampOn
+            currentLamp = .yellow
+        case .yellow:
+            red.alpha = lampOff
+            yellow.alpha = lampOn
+            currentLamp = .green
+        case .green:
+            green.alpha = lampOn
+            yellow.alpha = lampOff
+            currentLamp = .red
+        }
+    }
+    
 
 }
 
